a232d30c7543345ecb26f15aa555ed8f43e26238
adsrc <aditya16217@iiitd.ac.in>
2017-10-28 02:15:33 +0530

    Cimmit # Conflicts: #	build.gradle
    Added the Resource and PostComent class.

--- app/src/main/java/com/example/codeoff/PostComment.java
+++ app/src/main/java/com/example/codeoff/PostComment.java
@@ -5,4 +5,27 @@ package com.example.codeoff;
  */
 
 public class PostComment {
+
+
+    private User _commenter;
+    private String _text;
+
+
+    public User get_commenter() {
+        return _commenter;
+    }
+
+    public void set_commenter(User _commenter) {
+        this._commenter = _commenter;
+    }
+
+    public String get_text() {
+        return _text;
+    }
+
+    public void set_text(String _text) {
+        this._text = _text;
+    }
+
+
 }
