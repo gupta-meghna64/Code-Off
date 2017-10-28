b0eb50c14530904ff3487cf2627f88a66f16a2f1
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-10-28 09:58:43 +0530

    Resources extracted

--- app/src/main/java/com/example/codeoff/ResourcesFragment.java
+++ app/src/main/java/com/example/codeoff/ResourcesFragment.java
@@ -3,17 +3,27 @@ package com.example.codeoff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by HP on 28-10-2017.
  */
 
 public class ResourcesFragment extends Fragment {
+
+    private DatabaseReference mDatabase;
 
     @Nullable
     @Override
@@ -30,14 +40,54 @@ public class ResourcesFragment extends Fragment {
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Resources");
 
-        LinearLayout mainLayout = (LinearLayout) view.findViewById(R.id.verticalLayout);
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View v_1 = inflater.inflate(R.layout.inflator_resource,null);
-        v_1.setPadding(0,0,0,16);
-        View v_2 = inflater.inflate(R.layout.inflator_resource,null);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mainLayout.addView(v_1);
-        mainLayout.addView(v_2);
+        final LinearLayout mainLayout = (LinearLayout) view.findViewById(R.id.verticalLayout);
+        final LayoutInflater inflater = getActivity().getLayoutInflater();
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot d : dataSnapshot.child("Resource").getChildren()) {
+
+                    for (DataSnapshot e : d.getChildren()) {
+
+                        Resource r = e.getValue(Resource.class);
+                        View v = inflater.inflate(R.layout.inflator_resource, null);
+                        TextView heading = (TextView) v.findViewById(R.id.resourceName);
+                        TextView link = (TextView) v.findViewById(R.id.resourceLink);
+                        TextView name = (TextView) v.findViewById(R.id.uploaderName);
+                        TextView type = (TextView) v.findViewById(R.id.resourceType);
+                        TextView description = (TextView) v.findViewById(R.id.resourceDescription);
+                        heading.setText(r.get_title());
+                        link.setText(r.get_link());
+                        name.setText(r.get_uploader());
+                        type.setText(r.getType());
+                        description.setText(r.get_description());
+
+                        mainLayout.addView(v);
+                    }
+
+//                    LinearLayout commentsLayout = (LinearLayout) v.findViewById(R.id.scrollViewLayout);
+//
+//
+//                    for (int i = 0; i < r.get_comments().size(); i++) {
+//
+//                        TextView tv = new TextView(getActivity());
+//                        tv.setText(r.get_comments().get(i).get_text());
+//
+//
+//                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
