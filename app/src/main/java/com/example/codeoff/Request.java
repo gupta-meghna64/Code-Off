82b02b30768d3171ab75e341b68eabbb8fc0dcf5
adsrc <aditya16217@iiitd.ac.in>
2017-10-28 12:30:52 +0530

    pushed .xml file again

--- app/src/main/java/com/example/codeoff/Request.java
+++ app/src/main/java/com/example/codeoff/Request.java
@@ -32,11 +32,31 @@ public class Request {
         return _uploader;
     }
 
-
-
     public ArrayList<PostComment> get_comments() {
         return _comments;
     }
+
+
+    public void set_title(String _title) {
+        this._title = _title;
+    }
+
+    public void set_tags(ArrayList<String> _tags) {
+        this._tags = _tags;
+    }
+
+    public void set_description(String _description) {
+        this._description = _description;
+    }
+
+    public void set_uploader(String _uploader) {
+        this._uploader = _uploader;
+    }
+
+    public void set_comments(ArrayList<PostComment> _comments) {
+        this._comments = _comments;
+    }
+
 
 
 }
